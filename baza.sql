fecd59c4d15129b3d83ced137da9f86b4b9e535a
nooblet123 <32031662+nooblet123@users.noreply.github.com>
2018-05-28 17:27:08 +0200

    Add files via upload

--- baza.sql
+++ baza.sql
@@ -1,27 +1,48 @@
+drop table modele;
+drop table bagaze;
+
+
 
 CREATE TABLE MODELE
 (
-    id_modelu NUMBER(6,0) PRIMARY KEY;
-    marka VARCHAR2(20) NOT NULL;
-    liczba_siedzen NUMBER(3) NOT NULL; 
-    nazwa VARCHAR2(20) NOT NULL UNIQUE;
-)
+    id_modelu NUMBER(6,0) PRIMARY KEY,
+    marka VARCHAR2(20) NOT NULL,
+    liczba_siedzen NUMBER(3) NOT NULL, 
+    nazwa VARCHAR2(20) NOT NULL UNIQUE
+);
 
 CREATE TABLE BAGAZE
 (
     id_bagazu NUMBER(6,0) PRIMARY KEY,
     waga NUMBER(6,0) CHECK (waga > 0),
     nadbagaz NUMBER(1,0),
-    wymiar1 (4,0) CHECK (wymiar1 > 0),
-    wymiar2 (4,0) CHECK (wymiar2 > 0),
-    wymiar3 (4,0) CHECK (wymiar3 > 0),
-)
+    wymiar1 NUMBER(4,0) CHECK (wymiar1 > 0),
+    wymiar2 NUMBER(4,0) CHECK (wymiar2 > 0),
+    wymiar3 NUMBER(4,0) CHECK (wymiar3 > 0)
+);
 
 CREATE TABLE OBCE_LOTNISKA
 (
     id_lotniska NUMBER(6,0) PRIMARY KEY,
     nazwa_miasta VARCHAR(30) NOT NULL
-)
+);
+
+CREATE TABLE LINIE
+(
+    id_linii NUMBER(6,0) PRIMARY KEY,
+    limit_wymiar1 NUMBER(4,0) NOT NULL,
+    limit_wymiar2 NUMBER(4,0) NOT NULL,
+    limit_wymiar3 NUMBER(4,0) NOT NULL,
+    limit_wagi NUMBER(2,0) NOT NULL,
+    nazwa_linii VARCHAR2(40) NOT NULL
+);
+
+CREATE TABLE SAMOLOTY
+(
+    id_samolotu NUMBER(6,0) PRIMARY KEY,
+    id_linii NUMBER(6,0) NOT NULL REFERENCES LINIE(id_linii),
+    id_modelu NUMBER(6,0) NOT NULL REFERENCES MODELE(id_modelu)
+);
 
 CREATE TABLE OSOBY
 (
@@ -33,25 +54,17 @@ CREATE TABLE OSOBY
     email VARCHAR2(40),
     stanowisko VARCHAR2(30),
     id_linii NUMBER(6,0) REFERENCES LINIE(id_linii)
-)
+);
 
-CREATE TABLE LINIE
-(
-    id_linii NUMBER(6,0) PRIMARY KEY,
-    limit_wymiar1 NUMBER(4,0) NOT NULL,
-    limit_wymiar2 NUMBER(4,0) NOT NULL,
-    limit_wymiar3 NUMBER(4,0) NOT NULL,
-    limit_wagi NUMBER(2,0) NOT NULL,
-    nazwa_linii VARCHAR2(40) NOT NULL
-)
+
 
 CREATE TABLE TRASY 
 (
     id_trasy NUMBER(6,0) PRIMARY KEY,
     cena NUMBER(4,0),
-    id_obcego_lotniska NUMBER(6,0) REFERENCES lotniska(id_obcego_lotniska) ,
-    id_linii NUMBER(6,0) REFERENCES linii(id_linii)
-)
+    id_obcego_lotniska NUMBER(6,0) REFERENCES obce_lotniska(id_lotniska) ,
+    id_linii NUMBER(6,0) REFERENCES linie(id_linii)
+);
 
 CREATE TABLE UZYTKOWNICY
 (
@@ -63,12 +76,6 @@ CREATE TABLE UZYTKOWNICY
     id_linii NUMBER(6) REFERENCES LINIE(id_linii)
 );
 
-CREATE TABLE PRZYDZIALY
-(
-    id_lotu NUMBER(6) NOT NULL REFERENCES LOTY(id_lotu),
-    id_osoby NUMBER(6) NOT NULL REFERENCES OSOBY(id_osoby)
-)
-
 CREATE TABLE LOTY
 (
     id_lotu NUMBER(6,0) PRIMARY KEY,
@@ -77,7 +84,15 @@ CREATE TABLE LOTY
     status CHAR(1),
     id_samolotu NUMBER(6,0) NOT NULL REFERENCES SAMOLOTY(id_samolotu),
     id_trasy NUMBER(6,0) NOT NULL REFERENCES TRASY(id_trasy)
-)
+);
+
+CREATE TABLE PRZYDZIALY
+(
+    id_lotu NUMBER(6) NOT NULL REFERENCES LOTY(id_lotu),
+    id_osoby NUMBER(6) NOT NULL REFERENCES OSOBY(id_osoby)
+);
+
+
 
 CREATE TABLE BILETY
 (
@@ -86,5 +101,5 @@ CREATE TABLE BILETY
     id_lotu NUMBER(6,0) NOT NULL REFERENCES LOTY(id_lotu),
     siedzenie NUMBER(3) NOT NULL,
     id_bagazu NUMBER(6,0) REFERENCES BAGAZE(id_bagazu)
-)
+);
 
