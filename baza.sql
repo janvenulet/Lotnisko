ae436eb22bb55bd8c7eb293436e7fbbceef1593e
janvenulet <37171903+janvenulet@users.noreply.github.com>
2018-05-30 18:30:21 +0200

    Poprawione warunki bazy, wypełnianie do końca.
    Poprawione warunki w bazach, utworzone 4 statusy lotu na 4 przypadki 'o' opóźniony, 'z' zatwierdzony przez lotnisko, 'c' anulowany, 'w' wyleciał oraz NULL dla niezaakceptowanego.

--- baza.sql
+++ baza.sql
@@ -1,5 +1,7 @@
-/*
 
+--KWERENDA USUWAJACA WSZYSTKO:
+
+DROP VIEW widok;
 DROP TABLE REKLAMACJE;
 DROP TABLE UZYTKOWNICY;
 DROP TABLE PRZYDZIALY;
@@ -12,25 +14,17 @@ DROP TABLE TRASY;
 DROP TABLE MODELE;
 DROP TABLE LINIE;
 DROP TABLE OBCE_LOTNISKA;
-*/
+
+
 
 CREATE TABLE MODELE
 (
     id_modelu NUMBER(6,0) PRIMARY KEY,
     marka VARCHAR2(20) NOT NULL,
     liczba_siedzen NUMBER(3) NOT NULL CHECK (liczba_siedzen > 0), 
-    nazwa VARCHAR2(20) NOT NULL UNIQUE
+    nazwa VARCHAR2(30) NOT NULL UNIQUE
 );
 
-CREATE TABLE BAGAZE
-(
-    id_bagazu NUMBER(6,0) PRIMARY KEY,
-    waga NUMBER(6,0) CHECK (waga > 0),
-    nadbagaz NUMBER(1,0) DEFAULT 0,
-    wymiar1 NUMBER(4,0) CHECK (wymiar1 > 0),
-    wymiar2 NUMBER(4,0) CHECK (wymiar2 > 0),
-    wymiar3 NUMBER(4,0) CHECK (wymiar3 > 0)
-);
 
 CREATE TABLE OBCE_LOTNISKA
 (
@@ -62,7 +56,7 @@ CREATE TABLE OSOBY
     nazwisko VARCHAR(40) NOT NULL,
     wiek NUMBER(3,0) NOT NULL CHECK (wiek > 0 AND wiek <150),
     nr_paszportu VARCHAR2(10) UNIQUE,
-    email VARCHAR2(40) UNIQUE,
+    email VARCHAR2(40) UNIQUE CHECK ( email IS NULL OR email LIKE '%@%.%') ,
     stanowisko VARCHAR2(30),
     id_linii NUMBER(6,0) REFERENCES LINIE(id_linii)
 );
@@ -83,16 +77,16 @@ CREATE TABLE UZYTKOWNICY
     id_osoby NUMBER(6,0) NOT NULL,
     login VARCHAR2(20) NOT NULL UNIQUE,
     haslo VARCHAR2(20) NOT NULL,
-    tryb_dostepu NUMBER(1) DEFAULT 0 NOT NULL,
+    tryb_dostepu NUMBER(1) DEFAULT 0 NOT NULL CHECK ( tryb_dostepu IN (0,1,2,3)),
     id_linii NUMBER(6) REFERENCES LINIE(id_linii)
 );
 
 CREATE TABLE LOTY
 (
     id_lotu NUMBER(6,0) PRIMARY KEY,
-    data DATE NOT NULL,
-    bramka NUMBER(6,0),
-    status CHAR(1),
+    dataa DATE NOT NULL,
+    bramka NUMBER(6,0) CHECK ( bramka IS NULL OR bramka IN ( 1, 2, 3, 4, 5, 7)) ,
+    status CHAR(1) CHECK (status IN ('z', 'o' , 'c', 'w', NULL ) ),
     id_samolotu NUMBER(6,0) NOT NULL REFERENCES SAMOLOTY(id_samolotu),
     id_trasy NUMBER(6,0) NOT NULL REFERENCES TRASY(id_trasy)
 );
@@ -110,19 +104,28 @@ CREATE TABLE BILETY
     id_biletu NUMBER(6,0) PRIMARY KEY,
     id_osoby NUMBER(6,0) NOT NULL REFERENCES OSOBY(id_osoby),
     id_lotu NUMBER(6,0) NOT NULL REFERENCES LOTY(id_lotu),
-    siedzenie NUMBER(3) NOT NULL CHECK(siedzenie > 0),
-    id_bagazu NUMBER(6,0) REFERENCES BAGAZE(id_bagazu)
+    siedzenie NUMBER(3) NOT NULL CHECK(siedzenie > 0)
+);
+
+CREATE TABLE BAGAZE
+(
+    id_bagazu NUMBER(6,0) PRIMARY KEY,
+    waga NUMBER(6,0) CHECK (waga > 0),
+    nadbagaz NUMBER(1,0) DEFAULT 0,
+    wymiar1 NUMBER(4,0) CHECK (wymiar1 > 0),
+    wymiar2 NUMBER(4,0) CHECK (wymiar2 > 0),
+    wymiar3 NUMBER(4,0) CHECK (wymiar3 > 0),
+    id_biletu NUMBER(6,0) UNIQUE NOT NULL REFERENCES BILETY(id_biletu)
 );
 
 CREATE TABLE REKLAMACJE
 (
     id_reklamacje NUMBER(6,0),
     opis_reklamacji VARCHAR2(70), 
-    status_reklamacji NUMBER(1,0),
+    status_reklamacji NUMBER(1,0), 
     id_biletu NUMBER(6,0) NOT NULL REFERENCES BILETY (id_biletu)
 );
 
 CREATE VIEW WIDOK AS
 SELECT * FROM LOTY NATURAL JOIN TRASY;
-);
 
--- primarydata.sql
+++ primarydata.sql
@@ -1,34 +1,28 @@
 
 
---select *  FROM obce_lotniska;
---select *  FROM linie;
---delete FROM linie;
---delete FROM obce_lotniska;
-
-
-/*
-
 insert into OBCE_LOTNISKA (id_lotniska, nazwa_lotniska) VALUES (obce_lotniska_seq.NEXTVAL, 'Santiago');
 insert into OBCE_LOTNISKA (id_lotniska, nazwa_lotniska) VALUES (obce_lotniska_seq.NEXTVAL, 'Puebla');
-insert into OBCE_LOTNISKA (id_lotniska, nazwa_lotniska) VALUES (obce_lotniska_seq.NEXTVAL, 'Belfast');
+insert into OBCE_LOTNISKA (id_lotniska, nazwa_lotniska) VALUES (obce_lotniska_seq.NEXTVAL, 'Lizbona');
 insert into OBCE_LOTNISKA (id_lotniska, nazwa_lotniska) VALUES (obce_lotniska_seq.NEXTVAL, 'Alicante');
+insert into OBCE_LOTNISKA (id_lotniska, nazwa_lotniska) VALUES (obce_lotniska_seq.NEXTVAL, 'Cancun');
 insert into OBCE_LOTNISKA (id_lotniska, nazwa_lotniska) VALUES (obce_lotniska_seq.NEXTVAL, 'Lima');
+insert into OBCE_LOTNISKA (id_lotniska, nazwa_lotniska) VALUES (obce_lotniska_seq.NEXTVAL, 'Mexico City');
+insert into OBCE_LOTNISKA (id_lotniska, nazwa_lotniska) VALUES (obce_lotniska_seq.NEXTVAL, 'Vigo');
+insert into OBCE_LOTNISKA (id_lotniska, nazwa_lotniska) VALUES (obce_lotniska_seq.NEXTVAL, 'Sewilla');
+insert into OBCE_LOTNISKA (id_lotniska, nazwa_lotniska) VALUES (obce_lotniska_seq.NEXTVAL, 'Bogota');
+insert into OBCE_LOTNISKA (id_lotniska, nazwa_lotniska) VALUES (obce_lotniska_seq.NEXTVAL, 'Miami');
 
 
-*/
-
-
-insert into LINIE (id_linii ,limit_wymiar1 , limit_wymiar2 , limit_wymiar3, limit_wagi, nazwa_linii) VALUES (LINIE_seq.NEXTVAL, 120 , 50 , 42 , 20, 'LOT');
+insert into LINIE (id_linii ,limit_wymiar1 , limit_wymiar2 , limit_wymiar3, limit_wagi, nazwa_linii) VALUES (LINIE_seq.NEXTVAL, 120 , 50 , 42 , 20, 'Iberia');
 insert into LINIE (id_linii ,limit_wymiar1 , limit_wymiar2 , limit_wymiar3, limit_wagi, nazwa_linii) VALUES (LINIE_seq.NEXTVAL, 80 , 40 , 38 , 18, 'Peruvian Airlines');
-insert into LINIE (id_linii ,limit_wymiar1 , limit_wymiar2 , limit_wymiar3, limit_wagi, nazwa_linii) VALUES (LINIE_seq.NEXTVAL, 105 , 52 , 50 , 23, 'FlyCanada');
+insert into LINIE (id_linii ,limit_wymiar1 , limit_wymiar2 , limit_wymiar3, limit_wagi, nazwa_linii) VALUES (LINIE_seq.NEXTVAL, 105 , 52 , 50 , 23, 'LATAM Airlines');
 insert into LINIE (id_linii ,limit_wymiar1 , limit_wymiar2 , limit_wymiar3, limit_wagi, nazwa_linii) VALUES (LINIE_seq.NEXTVAL, 90 , 60 , 36 , 20, 'AirFrance');
+insert into LINIE (id_linii ,limit_wymiar1 , limit_wymiar2 , limit_wymiar3, limit_wagi, nazwa_linii) VALUES (LINIE_seq.NEXTVAL, 90 , 60 , 36 , 20, 'AeroMexico');
 
-insert into LINIE (id_linii ,limit_wymiar1 , limit_wymiar2 , limit_wymiar3, limit_wagi, nazwa_linii) VALUES (LINIE_seq.NEXTVAL, 90 , 60 , 36 , 20, 'Malaysia Airlines');
-
-INSERT INTO OSOBY VALUES(OSOBY_SEQ.NEXTVAL, 'Tomasz', 'Zi�ba', 25, 1234, 'tomasz@zieba.com', NULL, NULL);
+INSERT INTO OSOBY VALUES(OSOBY_SEQ.NEXTVAL, 'Tomasz', 'Zieba', 25, 1234, 'tomasz@zieba.com', NULL, NULL);
 INSERT INTO OSOBY VALUES(OSOBY_SEQ.NEXTVAL,'Andrzej', 'Krzak', 40, 1000, 'andrzej123@gmail.com', NULL, NULL);
-INSERT INTO OSOBY VALUES(OSOBY_SEQ.NEXTVAL, 'Zbigniew', 'Zimo�', 23, 53141, 'zzimon@hotmail.com', NULL, NULL);
-INSERT INTO OSOBY VALUES(OSOBY_SEQ.NEXTVAL, 'Anna', 'Grzyb', 11, 4141, 'agrz@gmail.com', NULL, NULL);
+INSERT INTO OSOBY VALUES(OSOBY_SEQ.NEXTVAL, 'Zbigniew', 'Zimoch', 23, 53141, 'zzimon@hotmail.com', NULL, NULL);
+INSERT INTO OSOBY VALUES(OSOBY_SEQ.NEXTVAL, 'Anna', 'Grzyb', 11, 4141, 'agrz@mit.usa', NULL, NULL);
 INSERT INTO OSOBY VALUES(OSOBY_SEQ.NEXTVAL, 'Joachim', 'Lew', 42, 5315, 'achimek1976@gmail.com', NULL, NULL);
 
 INSERT INTO UZYTKOWNICY VALUES(UZYTKOWNICY_SEQ.NEXTVAL, 4, 'pusia123', 'haslo123', 0, NULL);
@@ -37,17 +31,35 @@ INSERT INTO UZYTKOWNICY VALUES(UZYTKOWNICY_SEQ.NEXTVAL, 2, 'krzaczysko', 'ognisk
 INSERT INTO UZYTKOWNICY VALUES(UZYTKOWNICY_SEQ.NEXTVAL, 5, 'achim1976', 'haslo', 0, NULL);
 INSERT INTO UZYTKOWNICY VALUES(UZYTKOWNICY_SEQ.NEXTVAL, 1, 'tomzieb', 'l4tjklna', 0, NULL);
 
-INSERT INTO LOTY VALUES (LOTY_SEQ.NEXTVAL, '01-jan-2019', 0, 'd', 1, 1);
-INSERT INTO LOTY VALUES (LOTY_SEQ.NEXTVAL, '02-jun-2018', 0, 'd', 2, 3);
-INSERT INTO LOTY VALUES (LOTY_SEQ.NEXTVAL, '03-jun-2018', 0, 'd', 1, 2);
-INSERT INTO LOTY VALUES (LOTY_SEQ.NEXTVAL, '04-jun-2018', 0, 'd', 2, 2);
-INSERT INTO LOTY VALUES (LOTY_SEQ.NEXTVAL, '05-jun-2018', 0, 'd', 2, 4);
+INSERT INTO MODELE VALUES (MODELE_SEQ.NEXTVAL, 'Tupolew' , 210, 'Tu-204' );
+INSERT INTO MODELE VALUES (MODELE_SEQ.NEXTVAL, 'Airbus' , 150, 'Airbus A320' );
+INSERT INTO MODELE VALUES (MODELE_SEQ.NEXTVAL, 'Boeing' , 104, 'Boeing 737' );
+INSERT INTO MODELE VALUES (MODELE_SEQ.NEXTVAL, 'Airbus' , 560, 'Airbus A380' );
+INSERT INTO MODELE VALUES (MODELE_SEQ.NEXTVAL, 'Boeing' , 250, 'Boeing 787 Dreamliner' );
 
-INSERT INTO BILETY VALUES(BILETY_SEQ.NEXTVAL, 2, 1, 22, 4);
-INSERT INTO BILETY VALUES(BILETY_SEQ.NEXTVAL, 2, 2, 24, 5);
-INSERT INTO BILETY VALUES(BILETY_SEQ.NEXTVAL, 1, 3, 27, 1);
-INSERT INTO BILETY VALUES(BILETY_SEQ.NEXTVAL, 4, 1, 1, 2);
-INSERT INTO BILETY VALUES(BILETY_SEQ.NEXTVAL, 5, 2, 30, 3);
+INSERT INTO SAMOLOTY VALUES ( SAMOLOTY_SEQ.NEXTVAL, 2, 2);
+INSERT INTO SAMOLOTY VALUES ( SAMOLOTY_SEQ.NEXTVAL, 2, 5); 
+INSERT INTO SAMOLOTY VALUES ( SAMOLOTY_SEQ.NEXTVAL, 5, 4);
+INSERT INTO SAMOLOTY VALUES ( SAMOLOTY_SEQ.NEXTVAL, 1, 3);
+INSERT INTO SAMOLOTY VALUES ( SAMOLOTY_SEQ.NEXTVAL, 1, 4);
+
+INSERT INTO TRASY VALUES (TRASY_SEQ.NEXTVAL, 429 ,5, 1); /*aero to puebla */
+INSERT INTO TRASY VALUES (TRASY_SEQ.NEXTVAL, 429 ,6, 2); /*peruvian to lima */
+INSERT INTO TRASY VALUES (TRASY_SEQ.NEXTVAL, 429 ,2, 1); /*aero to puebla*/
+INSERT INTO TRASY VALUES (TRASY_SEQ.NEXTVAL, 429 ,7, 1); /*aero to mexcity */
+INSERT INTO TRASY VALUES (TRASY_SEQ.NEXTVAL, 429 ,1, 3); /*latam to santiago*/
+
+INSERT INTO LOTY VALUES (LOTY_SEQ.NEXTVAL, to_date('20180623 11:20','yyyymmdd hh24:mi'), 2, 'z', 1, 1); /*bramka status samolot trasa*/
+INSERT INTO LOTY VALUES (LOTY_SEQ.NEXTVAL, to_date('20180703 9:50','yyyymmdd hh24:mi'), 2, NULL, 2, 3);
+INSERT INTO LOTY VALUES (LOTY_SEQ.NEXTVAL, to_date('20180619 15:00','yyyymmdd hh24:mi'), 3, 'z', 1, 1);
+INSERT INTO LOTY VALUES (LOTY_SEQ.NEXTVAL, to_date('20180618 11:20','yyyymmdd hh24:mi'), 1, 'z', 2, 2);
+INSERT INTO LOTY VALUES (LOTY_SEQ.NEXTVAL, to_date('20180619 18:20','yyyymmdd hh24:mi'), 5, 'c', 2, 4);
+
+INSERT INTO BILETY VALUES(BILETY_SEQ.NEXTVAL, 2, 1, 22);
+INSERT INTO BILETY VALUES(BILETY_SEQ.NEXTVAL, 2, 2, 24);
+INSERT INTO BILETY VALUES(BILETY_SEQ.NEXTVAL, 1, 3, 27);
+INSERT INTO BILETY VALUES(BILETY_SEQ.NEXTVAL, 4, 1, 1);
+INSERT INTO BILETY VALUES(BILETY_SEQ.NEXTVAL, 5, 2, 30);
 
 
 INSERT INTO REKLAMACJE VALUES(REKLAMACJE_SEQ.NEXTVAL, 'obsluga byla niemila', 0, 1);
@@ -55,19 +67,14 @@ INSERT INTO REKLAMACJE VALUES(REKLAMACJE_SEQ.NEXTVAL, 'samolot nie polecial', 0,
 INSERT INTO REKLAMACJE VALUES(REKLAMACJE_SEQ.NEXTVAL, 'zupa byla za slona', 0, 3);
 
 
-INSERT INTO BAGAZE VALUES(BAGAZE_SEQ.NEXTVAL, 5, default, 3, 2, 1);
-INSERT INTO BAGAZE VALUES(BAGAZE_SEQ.NEXTVAL, 7, default, 3, 3, 3);
-INSERT INTO BAGAZE VALUES(BAGAZE_SEQ.NEXTVAL, 5, default, 10, 10, 5);
-INSERT INTO BAGAZE VALUES(BAGAZE_SEQ.NEXTVAL, 3, default, 4, 10, 15);
-INSERT INTO BAGAZE VALUES(BAGAZE_SEQ.NEXTVAL, 5, default, 3, 5, 11);
+INSERT INTO BAGAZE VALUES(BAGAZE_SEQ.NEXTVAL, NULL, default, 103, 21, 16, 1);
+INSERT INTO BAGAZE VALUES(BAGAZE_SEQ.NEXTVAL, 15, default, 13, 33, 37, 3);
+INSERT INTO BAGAZE VALUES(BAGAZE_SEQ.NEXTVAL, 5, default, 10, 10, 5, 4);
+INSERT INTO BAGAZE VALUES(BAGAZE_SEQ.NEXTVAL, NULL, default, 4, 10, 15, 5);
+INSERT INTO BAGAZE VALUES(BAGAZE_SEQ.NEXTVAL, 5, default, 3, 5, 11, 2);
 
 INSERT INTO PRZYDZIALY VALUES (1,1);
 INSERT INTO PRZYDZIALY VALUES (1,2);
 INSERT INTO PRZYDZIALY VALUES (2,1);
 INSERT INTO PRZYDZIALY VALUES (2,3);
 INSERT INTO PRZYDZIALY VALUES (3,4);
-
-select * from bagaze;
-
-
-
