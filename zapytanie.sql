890bc5a07de12288063ae6737eb3de6d968b6778
nooblet123 <32031662+nooblet123@users.noreply.github.com>
2018-06-01 14:14:09 +0200

    Add files via upload

--- zapytanie.sql
+++ zapytanie.sql
@@ -1,26 +1,91 @@
 /*
 To zapytanie rejestruje osobê, nastepnie trzeba uruchomiæ zapytanie rejestrujace odp. uzytkownika
 Tutaj pytajniki to odp. pola z formularza rejestracyjnego (IMIE, NAZWISKO, WIEK, NR PASZPORTU, EMAIL)
-Dobrze byloby zweryfikowac poprawnoœæ pól
+Dobrze byloby zweryfikowac poprawnoœæ pól po stronie programu
 */
 insert into osoby values(OSOBY_SEQ.Nextval, ?, ?, ?, ?, ?, null, null);
 
 /*
 To zapytanie 
-PYTAJNIKI TO POLA Z FORMULARZA REJESTRACYJNEGO (LOGIN, HASLO)
+Pytajniki to pola z formularza rejestracyjnego (login, haslo)
+Mo¿na dodaæ weryfikacjê loginu i hasla (np. dlugosc, znaki specjalne) po stronie programu
 */
 insert into uzytkownicy values(UZYTKOWNICY_SEQ.Nextval, OSOBY_SEQ.Currval, ?, ?, 0,null);
 
 /*
-TO ZAPYTANIE S£U¯Y DO TWORZENIA ROZWIJANEJ LISTY BILETÓW MO¯LIWYCH DO REKLAMOWANIA.
-PYTAJNIK TO ID OSOBY ZALOGOWANEGO U¯YTKOWNIKA (ZAK£ADAM, ¯E TE DANE S¥ PRZECHOWYWANE W INTERFEJSIE)
+To zapytanie sluzy do wyswietlenia listy przylotow
 */
+select nazwa_lotniska, data, status 
+from loty l join trasy t
+on l.id_trasy = t.id_trasy
+join obce_lotniska x
+on t.id_lotniska = x.id_lotniska
+where status = 'ap';
+
+/*
+To zapytanie sluzy do wyswietlenia listy odlotow
+*/
+select nazwa_lotniska, data, status 
+from loty l join trasy t
+on l.id_trasy = t.id_trasy
+join obce_lotniska x
+on t.id_lotniska = x.id_lotniska
+where status = 'ao';
+
+
+/*
+To zapytanie sluzy do wytworzenia rozwijanej listy biletow mozliwych do reklamacji.
+Pytajnik to id_osoby zalogowanego uzytkownika (Zakladam, ze te dane sa przechowywane w programie)
+Wybór odpowiedniego pola wskazuje id_biletu, na który zgloszona bedzie reklamacja
+*/
+
 select nazwa_lotniska, data 
 from loty l join trasy t
 on l.id_trasy = t.id_trasy
 join obce_lotniska x
-on t.id_obcego_lotniska = x.id_obcego_lotniska;
-/*join bilety b on b.id_lotu = l_id_lotu*/
-/*where b.id_osoby = ?*/
+on t.id_lotniska = x.id_lotniska
+join bilety b on b.id_lotu = l.id_lotu
+where b.id_osoby = ?;
 
-insert into reklamacje values (REKLAMACJE_SEQ.Nextval, ?, 0, 
+/*
+To zapytanie obsluguje formularz reklamacyjny
+Pytajniki to kolejno: pole opisu formularza, id_biletu wybrane z listy rozwijanej
+*/
+insert into reklamacje values (REKLAMACJE_SEQ.Nextval, ?, 0, ?);
+
+
+/*
+To zapytanie sprawdza status reklamacji
+Pytajnik to id_reklamacji uzyskane w jakis sposob (jaki?)
+*/
+
+select status_reklamacji from reklamacje
+where id_reklamacji = ?;
+
+/*
+To podzapytanie obsluguje wyszukanie lotu wedlug kryteriow
+Pytajniki to: dzien, miesiac, rok, cel (nazwa_lotniska), cena_min, cena_max
+Format ustawiæ na YY lub YYYY w zale¿noœci od sposobu zapisu w programie
+
+*/
+select l.data, cena, nazwa_linii, nazwa_lotniska
+from loty l join trasy t
+on l.id_trasy = t.id_trasy
+join obce_lotniska x
+on t.id_lotniska = x.id_lotniska
+join linie n
+on t.id_linii = n.id_linii
+where (trunc(l.data) = trunc(to_date(? || ? || ?, 'DDMMYY')))
+AND nazwa_lotniska = ?
+AND cena > ?
+AND cena < ?;
+
+/*
+To podzapytanie obsluguje zakup biletu
+id_lotu pochodzi z poprzedniego okienka
+id_osoby jest przechowywane w programie
+Pytajniki kolejno to: id_osoby, id_lotu, siedzenie (siedzenie obliczane w programie?)
+id_bagazu zostanie dodane dopiero przy nadawaniu bagazu na lotnisku
+*/
+INSERT INTO BILETY VALUES(BILETY_SEQ.NEXTVAL, ?, ?, ?,null);
+
